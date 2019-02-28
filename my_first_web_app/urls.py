e5b854203d485ee72a77e35a44964e171999224e
http://www.github.com/mimsbot <mimseyfield@gmail.com>
2019-02-28 16:34:50 -0500

    added ranom images to gallery

--- my_first_web_app/urls.py
+++ my_first_web_app/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from random import randint
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import path
@@ -21,13 +21,16 @@ from django.urls import path
 
 def home_page(request):
     # http://127.0.0.1:8000/
-    response = render(request, 'index.html')
+    context = {'name': 'Mims Bot'}
+    response = render(request, 'index.html', context)
     return HttpResponse(response)
 
 
 def portfolio(request):
-    # http://127.0.0.1:8000/
-    response = render(request, 'gallery.html')
+    random_number = randint(0, 100)
+    image_url = "https://picsum.photos/400/600/?image={}".format(random_number)
+    context = {'gallery_image': image_url}
+    response = render(request, 'gallery.html', context)
     return HttpResponse(response)
 
 
